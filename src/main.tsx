cf67b62ef17a1d10f91cf3a45436579d3706c64e
matikurland-oss <matikurland@gmail.com>
2026-03-21 08:45:08 +0200

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,20 @@
-import {StrictMode} from 'react';
-import {createRoot} from 'react-dom/client';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import App from './App.tsx';
+import ContactPage from './ContactPage.tsx';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        {/* דף הבית */}
+        <Route path="/" element={<App />} />
+        
+        {/* דף צור קשר */}
+        <Route path="/contact" element={<ContactPage />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 );
