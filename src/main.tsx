4a28c0518fdc9def87c8a97309a0b9ea36bb2297
matikurland-oss <matikurland@gmail.com>
2026-03-21 10:40:57 +0200

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,8 @@ import { createRoot } from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import App from './App.tsx';
 import ContactPage from './ContactPage.tsx';
+import BlogPage from './BlogPage.tsx'; // ייבוא חדש
+import BlogPostDetail from './BlogPostDetail.tsx'; // ייבוא חדש
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
@@ -14,6 +16,10 @@ createRoot(document.getElementById('root')!).render(
         
         {/* דף צור קשר */}
         <Route path="/contact" element={<ContactPage />} />
+
+        {/* דפי הבלוג של CleanFry */}
+        <Route path="/blog" element={<BlogPage />} />
+        <Route path="/blog/:id" element={<BlogPostDetail />} />
       </Routes>
     </BrowserRouter>
   </StrictMode>,
