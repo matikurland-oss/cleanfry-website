bb0dbba9ac002401787cb38a74e0527c92d0ba21
matikurland-oss <matikurland@gmail.com>
2026-03-21 22:28:28 +0200

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,23 +1,13 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App.tsx';
-import ContactPage from './ContactPage.tsx';
-import BlogPage from './BlogPage.tsx';
-import BlogPostDetail from './BlogPostDetail.tsx';
-import SuccessPage from './SuccessPage.tsx'; // השורה שחסרה לך!
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/contact" element={<ContactPage />} />
-        <Route path="/blog" element={<BlogPage />} />
-        <Route path="/blog/:id" element={<BlogPostDetail />} />
-        <Route path="/thanks" element={<SuccessPage />} />
-      </Routes>
+      <App />
     </BrowserRouter>
   </StrictMode>
 );
