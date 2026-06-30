81a7250b4fe762ba14d7037816700100ca05a470
matikurland-oss <matikurland@gmail.com>
2026-06-30 10:14:30 +0300

    Update CheckoutPage.tsx

--- src/CheckoutPage.tsx
+++ src/CheckoutPage.tsx
@@ -24,7 +24,7 @@ const CheckoutPage = () => {
 
   // הגדרות מחיר
   const UNIT_PRICE = 59;
-  const SHIPPING_COST = 39;
+  const SHIPPING_COST = 35;
   const FREE_SHIPPING_THRESHOLD = 299;
 
   // זיהוי כמות מה-URL
