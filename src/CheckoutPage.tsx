8dd2a88c24edb8cf0d6b69ff0ab5b391348eebcd
matikurland-oss <matikurland@gmail.com>
2026-07-18 09:24:16 +0300

    Update CheckoutPage.tsx

--- src/CheckoutPage.tsx
+++ src/CheckoutPage.tsx
@@ -36,8 +36,8 @@ const CheckoutPage = () => {
   const [showPayment, setShowPayment] = useState(false);
 
   // 2. הגדרות מחיר (המחיר האמיתי של מארז הוא 59 ש"ח)
-  const UNIT_PRICE = 1; 
-  const SHIPPING_COST = 0;
+  const UNIT_PRICE = 59; 
+  const SHIPPING_COST = 35;
   const FREE_SHIPPING_THRESHOLD = 249;
   const FORMSPREE_URL = "https://formspree.io/f/xvzwnrla";
 
